a8afd18f7c765ca096985f1547fcbf8b7e0994c9
April Monk <123050452+AprilMonk@users.noreply.github.com>
2023-06-21 17:09:40 -0500

    more work to do

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -1,29 +1,29 @@
 /* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
 let products = [{
-  id:"cherry",
+  productId: "cherry",
   name: "Cherries",
-  price: "5",
+  price: 5,
   desc: "Fresh Bright Red cherries!",
   image: "images/cherry.jpg",
-  quantity: "0",
-},{
-id: "strawberry",
-name:"Strawberries",
-price: "4",
-desc: "Fresh strawberries!",
-image: "images/strawberry.jpg",
-quantity: "0", 
-},{
-  id: "orange",
-  name:"Oranges",
-  price: "10",
+  quantity: 0,
+}, {
+  productId: "strawberry",
+  name: "Strawberries",
+  price: 4,
+  desc: "Fresh strawberries!",
+  image: "images/strawberry.jpg",
+  quantity: 0,
+}, {
+  productId: "orange",
+  name: "Oranges",
+  price: 10,
   desc: "Fresh Oranges right from the tree!",
   image: "images/orange.jpg",
-  quantity: "0",
+  quantity: 0,
 }];
-let increment = () =>{};
-let decrement = () => {};
-let update = () => {};
+let increment = () => { };
+let decrement = () => { };
+let update = () => { };
 
 /* Create 3 or more product objects using object literal notation 
    Each product should include five properties
@@ -34,61 +34,60 @@ let update = () => {};
    - image: picture of product (url string)
 */
 
-/* Images provided in /images folder. All images from Unsplash.com
-   - cherry.jpg by Mae Mu
-   - orange.jpg by Mae Mu
-   - strawberry.jpg by Allec Gomes
-*/
+let cart = [];
 
-/* Declare an empty array named cart to hold the items in the cart */
-let cart= [];
-/* Create a function named addProductToCart that takes in the product productId as an argument
+/* addProductToCart takes in the product productId as an argument
   - addProductToCart should get the correct product based on the productId
   - addProductToCart should then increase the product's quantity
   - if the product is not already in the cart, add it to the cart
 */
-const addProductToCart = (id) =>{
-  const index = cart.findIndex(item => item.id === id);
+const addProductToCart = id => {
+  const index = cart.findIndex(item => item.productId === id);
   if (index === -1) {
-    cart.push ()
+    const productToAdd = products.findIndex(item => item.productId === id);
+    cart.push(products[productToAdd]);
+  } else {
+    cart[index].quantity += 1;
   }
 }
-/* Create a function named increaseQuantity that takes in the productId as an argument
+
+/* increaseQuantity takes in the productId as an argument
   - increaseQuantity should get the correct product based on the productId
   - increaseQuantity should then increase the product's quantity
 */
 
-/* Create a function named decreaseQuantity that takes in the productId as an argument
+/* decreaseQuantity takes in the productId as an argument
   - decreaseQuantity should get the correct product based on the productId
   - decreaseQuantity should decrease the quantity of the product
   - if the function decreases the quantity to 0, the product is removed from the cart
 */
+function decreaseQuantity(id) {
+  // decrement
 
-/* Create a function named removeProductFromCart that takes in the productId as an argument
+}
+
+/* removeProductFromCart takes in the productId as an argument
   - removeProductFromCart should get the correct product based on the productId
   - removeProductFromCart should update the product quantity to 0
   - removeProductFromCart should remove the product from the cart
 */
+function removeProductFromCart(id) {
+  // Remove the product
+}
 
-/* Create a function named cartTotal that has no parameters
+/* cartTotal has no parameters
   - cartTotal should iterate through the cart to get the total of all products
   - cartTotal should return the sum of the products in the cart
 */
-const cartTotal = () => {};
+const cartTotal = () => { };
 
 
-/* Create a function called emptyCart that empties the products from the cart */
+/* emptyCart empties the products from the cart */
 
-/* Create a function named pay that takes in an amount as an argument
+/* pay takes in an amount as an argument
   - pay will return a negative number if there is a remaining balance
   - pay will return a positive number if money should be returned to customer
 */
-
-/* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
-
-
-/* The following is for running unit tests. 
-   To fully complete this project, it is expected that all tests pass.
-   Run the following command in terminal to run tests
-   npm run test
-*/
+function pay(amount) {
+  // do math here
+}
